99fad917fc6a71be3bce89e03d39556af3fc778a
RD Bhardwaj <rbhardwaj90@iitbombay.org>
2026-01-04 16:46:59 +0530

    Initial Commit

---
+++ Aro-CLI/Aro_CLIApp.swift
@@ -0,0 +1,17 @@
+//
+//  Aro_CLIApp.swift
+//  Aro-CLI
+//
+//  Created by RD Bhardwaj on 04/01/26.
+//
+
+import SwiftUI
+
+@main
+struct Aro_CLIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
